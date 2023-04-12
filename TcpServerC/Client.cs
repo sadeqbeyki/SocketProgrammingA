29194880fcbc7723860950356c535e36aeb4ae86
mardibak <mardibaki@gmail.com>
2023-04-12 22:45:29 +0330

    tcpServerC Implemented, implemented tcpserver with console and wpf

---
+++ TcpServerC/Client.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using TcpServerC.Net.IO;
+
+namespace TcpServerC
+{
+    internal class Client
+    {
+        public string Username { get; set; }
+        public Guid UID { get; set; }
+        public TcpClient ClientSocket { get; set; }
+
+        PacketReader _packetReader;
+
+        public Client(TcpClient client)
+        {
+            ClientSocket = client;
+            UID = Guid.NewGuid();
+            _packetReader = new PacketReader(ClientSocket.GetStream());
+
+            var opCode = _packetReader.ReadByte();
+            Username = _packetReader.ReadMessage();
+
+            Console.WriteLine($"[{DateTime.Now}]: Client has connected with the userame: {Username}");
+
+            Task.Run(() => Process());
+        }
+
+        void Process()
+        {
+            while (true)
+            {
+                try
+                {
+                    var opcode = _packetReader.ReadByte();
+                    switch (opcode)
+                    {
+                        case 5:
+                            var message = _packetReader.ReadMessage();
+                            Console.WriteLine($"[{DateTime.Now}]: Message resived! {message}");
+                            Program.BroadcastMessage($"[{DateTime.Now}]: [{Username}]: [{message}]");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"[{UID.ToString()}]: Disconnected! ");
+                    Program.BroadcastDisconnect(UID.ToString());
+                    ClientSocket.Close();
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ TcpServerC/Net/IO/PacketBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServerC.Net.IO
+{
+    internal class PacketBuilder
+    {
+        MemoryStream memoryStream;
+
+        public PacketBuilder()
+        {
+            memoryStream = new MemoryStream();
+        }
+        public void WriteOpCode(byte opcode)
+        {
+            memoryStream.WriteByte(opcode);
+        }
+
+        public void WriteMessage(string message)
+        {
+            var msgLength = message.Length;
+            memoryStream.Write(BitConverter.GetBytes(msgLength));
+            memoryStream.Write(Encoding.ASCII.GetBytes(message));
+        }
+
+        public byte[] GetPacketBytes()
+        {
+            return memoryStream.ToArray();
+        }
+    }
+}
